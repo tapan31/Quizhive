64b57018250e4445f98b50aaa123b540ef5918e6
Tapan <tapansonak786@gmail.com>
2024-12-30 17:37:06 +0530

    Added sort feature on quiz history page

--- src/pages/QuizHistory.jsx
+++ src/pages/QuizHistory.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Button from "../components/Button";
 import HistoryCard from "../components/HistoryCard";
 import { useQuizContext } from "../contexts/QuizContext";
@@ -5,6 +6,30 @@ import { Link } from "react-router-dom";
 
 export default function QuizHistory() {
   const { quizHistory } = useQuizContext();
+
+  const [sortBy, setSortBy] = useState("default");
+
+  let sortedHistory = [];
+
+  if (sortBy === "score") {
+    sortedHistory = quizHistory
+      .slice()
+      .sort((a, b) => b.scorePercentage - a.scorePercentage);
+  } else if (sortBy === "questions") {
+    sortedHistory = quizHistory
+      .slice()
+      .sort((a, b) => b.numQuestions - a.numQuestions);
+  } else if (sortBy === "time") {
+    sortedHistory = quizHistory
+      .slice()
+      .sort((a, b) => b.timeTaken - a.timeTaken);
+  } else {
+    sortedHistory = quizHistory;
+  }
+
+  const handleChange = (e) => {
+    setSortBy(e.target.value);
+  };
 
   if (quizHistory?.length === 0) {
     return (
@@ -35,8 +60,30 @@ export default function QuizHistory() {
         <Button type="clearHistory">Clear All</Button>
       </div>
 
-      <ul className="mt-10 flex flex-col flex-wrap items-center justify-center gap-4 md:flex-row md:gap-7">
-        {quizHistory.map((quiz) => (
+      {/* Sort Order Menu */}
+      <div className="mt-5 flex flex-col items-center justify-between gap-2 md:flex-row md:justify-end md:gap-4">
+        <label
+          htmlFor="sort"
+          className="text-lg font-medium text-light-text md:text-xl dark:text-dark-text"
+        >
+          Sort By
+        </label>
+        <select
+          name="sort"
+          id="sort"
+          className="w-full rounded-full border border-primary bg-light-neutral px-4 py-2 text-light-text transition-colors focus:border-primary focus:outline-none focus:ring-2 focus:ring-primary md:w-44 dark:bg-dark-neutral dark:text-dark-text"
+          value={sortBy}
+          onChange={(e) => handleChange(e)}
+        >
+          <option value="default">Default</option>
+          <option value="score">Score</option>
+          <option value="questions">Number of Questions</option>
+          <option value="time">Time Taken</option>
+        </select>
+      </div>
+
+      <ul className="mt-10 flex flex-col flex-wrap items-center justify-start gap-4 md:flex-row md:gap-7">
+        {sortedHistory.map((quiz) => (
           <HistoryCard key={quiz.id} quiz={quiz} />
         ))}
       </ul>
